0b8cffc9a6a84e720076b9053070d52e990541b8
vai na web <vainaweb@gmail.com>
2019-05-30 17:10:46 -0300

    ajustando menu

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+$(function(){
+	/*=====|Menu Mobile|=====*/
+	$('.mobileButton').click(function(){
+		$('.menuMobile__block').css('display','block');
+	});
+	$('.buttonexit').click(function(){
+		$('.menuMobile__block').css('display','none');
+	});
+	$('.contatos').click(function(){
+		$('.menuMobile__block').css('display','none');
+	});
+});
